5df1a2d8cf6353f783f55be80de33fdc998db43a
SOYEONG OH <axionoso@gmail.com>
2019-03-22 20:03:28 +0000

    Automatically backed up by Learn

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -1,9 +1,3 @@
 def my_collect(collection)
-  counter = 0
-  new_array = []
-  while counter < collection.length
-    new_array << yield(collection[counter])
-    counter += 1
-  end
-  new_array
+
 end

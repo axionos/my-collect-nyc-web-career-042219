98fb5c70a46deef7af90d31df53be608e537dd6d
SOYEONG OH <axionoso@gmail.com>
2019-03-22 16:11:09 +0000

    Done.

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -1,2 +1,19 @@
+def my_collect(collection)
+  counter = 0
+  new_array = []
+  while counter < collection.length
+    new_array << yield(collection[counter])
+    counter += 1
+  end
+  new_array
+end
 
+# collection = ['ruby', 'javascript', 'python', 'objective-c']
+# my_collect(collection) do |lang|
+#   lang.upcase
+# end
 
+# array = ["Tim Jones", "Tom Smith", "Jim Campagno"]
+# my_collect(array) do |name|
+#   name.split(" ").first
+# end

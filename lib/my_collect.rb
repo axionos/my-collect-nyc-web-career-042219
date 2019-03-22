c358070883de9065077fa95cf05fdf3e060ef387
SOYEONG OH <axionoso@gmail.com>
2019-03-22 16:18:11 +0000

    Done.

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -7,13 +7,3 @@ def my_collect(collection)
   end
   new_array
 end
-
-# collection = ['ruby', 'javascript', 'python', 'objective-c']
-# my_collect(collection) do |lang|
-#   lang.upcase
-# end
-
-# array = ["Tim Jones", "Tom Smith", "Jim Campagno"]
-# my_collect(array) do |name|
-#   name.split(" ").first
-# end
